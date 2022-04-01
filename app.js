5997450459c1b10a35c0f038f6bb90839d88f8dd
BrunoBuresta <brunoburesta07@gmail.com>
2022-04-01 12:30:13 -0300

    levante el servidor en el puerto 4000

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen(4000, () =>{
+    console.log('servidor corriendo en http://localhost:4000');
+});
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+});
