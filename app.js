507a6a807143ad241cb77f72497355fb427bda9b
TomyHenderson <tomyhenderson@gmail.com>
2022-04-03 20:32:09 -0300

    Cree los archivos para el carrito

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ app.listen(4000, () =>{
 app.get("/", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 });
+
+app.get("/carritoDeCompras", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/carritoDeCompras.html"))
+});
+
+
