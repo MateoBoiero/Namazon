4b6f781dac0f3657847c7097e85734a0c03f491a
MateoBoiero <mateoboiero@hotmail.com>
2022-04-11 12:48:23 -0300

    agregando forgot

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.get("/detalleDeProducto", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "./views/detalleDeProducto.html"))
 });
 
+//Mateo
 app.get ("/login", (req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+app.get ("/register", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get ("/forgot", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/forgot.html"))
 });
