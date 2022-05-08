e84ffe3a1f834dd58acda1e0a7d8449f371d5049
BrunoBuresta <brunoburesta07@gmail.com>
2022-05-08 15:37:14 -0300

    agregue y requeri los partials en las viewsy cree el archivo retro

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const mainRouter = require("./routes/mainRouter");
+const mainRouter = require("./src/routes/mainRouter");
 app.set('views', path.resolve(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
