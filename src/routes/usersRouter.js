3af2578e1cd49928b14dd388279f8463dda7ca2d
MateoBoiero <mateoboiero@hotmail.com>
2022-05-18 20:32:10 -0300

    intentando solucionar css

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -7,8 +7,10 @@ const userController = require('../controllers/userController')
 
 //**!      CONTROLLERS    */
 router.get('/login',userController.login);
+
 router.get('/register',userController.register);
 router.post('/register',userController.processRegister);
+
 router.get('/forgot',userController.forgot);
 
 module.exports = router;
